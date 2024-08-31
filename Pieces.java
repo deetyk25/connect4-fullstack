ce1aaa657b97e774b4ce3265d46e5a579a393141
Deetya Kamat <133281509+deetyk25@users.noreply.github.com>
2024-08-30 17:50:09 -0700

    Create Pieces.java

---
+++ Pieces.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Pieces {
+    private String color;
+
+    //getter
+    public String getColor() {
+        return color;
+    }
+
+    //setter
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+}
